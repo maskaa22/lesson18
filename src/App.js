8bc35d08127379b236a312e09ca5f265f9ade296
MASHa <MA77777SHa>
2021-07-10 00:58:05 +0300

    /users/USER_ID/allposts

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import {BrowserRouter as Router,
-        Switch,
         Route,
         Link
 } from "react-router-dom";
 import './App.css';
 import Users from "./components/users/Users";
-import Posts from "./components/posts/Posts";
+
 
 
 function App() {
@@ -16,15 +15,8 @@ function App() {
           <div>
             <Link to={'/users'}>User page</Link>
           </div>
-          <div>
-             <Link to={'/posts'}>Post page</Link>
-          </div>
 
-          
-          <Switch>
               <Route path={'/users'} component={Users}/>
-              <Route path={'/posts'} component={Posts}/>
-          </Switch>
         </Router>
     </div>
   );
--- src/components/user/User.js
+++ src/components/user/User.js
@@ -1,14 +1,12 @@
 import {Link, Route} from "react-router-dom";
 import {useState} from "react";
 import {getPost} from "../servises/PostServise";
-import Post from "../post/Post";
 
 export default function User ({item})
 {
     const [posts, setPosts] = useState([]);
     const getPosts=()=>{
         getPost(item.id).then(value => {
-            //console.log(value.data);
             setPosts([...value.data])
         })
     }
@@ -17,13 +15,16 @@ export default function User ({item})
             {item.name} -
             <Link to={{
                 pathname: '/users/' + item.id + '/allposts',
-                state: item
+                //state: item
         }}>Show posts</Link>
 
                 <Route path={'/users/' + item.id + '/allposts'}>
                     {getPosts()}
                     {
-                        posts.map(value => <Post item={value}/>)
+                        posts.map((value) =>
+                        {
+                           return <div key={value.id}>{value.title}</div>
+                        })
                     }
                 </Route>
 
